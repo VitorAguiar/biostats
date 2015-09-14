3269048b104bd2cecfff3f4b321723494e35c5ef
Vitor Aguiar <vitor.aguiar@me.com>
2015-09-14 13:37:47 -0300

    return a matrix with results

--- R/odds_ratio.R
+++ R/odds_ratio.R
@@ -1,37 +1,34 @@
-odds_ratio <- function(data, alpha = 0.05, nrows = 2) {
+odds_ratio <- function(data, alpha = 0.05) {
 	   
-  for (i in 1:nrow(data)) {
+  results <- cbind(data, "odds_ratio" = NA, "ci_low" = NA, "ci_upp" = NA) 
+  results[1, "odds_ratio"] <- 1
+  cases_unexposed <- data[1, "cases"]
+  controls_unexposed <- data[1, "controls"]
+  
+  for (i in 2:nrow(data)) {
 
-    DiseaseUnexposed <- data[nrows,1]
-    ControlUnexposed <- data[nrows,2]
-
-    if (i < nrows) {
-	
-      DiseaseExposed <- data[i,1]
-      ControlExposed <- data[i,2]
-      totExposed <- DiseaseExposed + ControlExposed
-      totUnexposed <- DiseaseUnexposed + ControlUnexposed
-      
-      probDiseaseGivenExposed <- DiseaseExposed/totExposed
-      probDiseaseGivenUnexposed <- DiseaseUnexposed/totUnexposed
-      probControlGivenExposed <- ControlExposed/totExposed
-      probControlGivenUnexposed <- ControlUnexposed/totUnexposed
-      
-      oddsRatio <- (probDiseaseGivenExposed*probControlGivenUnexposed)/
-		    (probControlGivenExposed*probDiseaseGivenUnexposed)
-       
-      print(paste0("category = ", rownames(data)[i], 
-		   ", odds ratio = ", oddsRatio))
-      
-      confidenceLevel <- (1 - alpha)*100
-      sigma <- sqrt((1/DiseaseExposed)+(1/ControlExposed)+
-		    (1/DiseaseUnexposed)+(1/ControlUnexposed))
-       
-      z <- qnorm(1 - (alpha/2))
-      ci <- oddsRatio * c(exp(-z * sigma), exp( z * sigma))
-      
-      print(paste0("category = ", rownames(data)[i], ", ", confidenceLevel, 
-		   "% confidence interval = [", ci[1], ", ", ci[2], "]"))
-    }
+    cases_exposed <- data[i, "cases"]
+    controls_exposed <- data[i, "controls"]
+    totExposed <- cases_exposed + controls_exposed
+    totUnexposed <- cases_unexposed + controls_unexposed
+    
+    probDiseaseGivenExposed <- cases_exposed/totExposed
+    probDiseaseGivenUnexposed <- cases_unexposed/totUnexposed
+    probControlGivenExposed <- controls_exposed/totExposed
+    probControlGivenUnexposed <- controls_unexposed/totUnexposed
+    
+    odds_ratio <- (probDiseaseGivenExposed*probControlGivenUnexposed)/
+		  (probControlGivenExposed*probDiseaseGivenUnexposed)
+     
+    confidenceLevel <- (1 - alpha)*100
+    sigma <- sqrt((1/cases_exposed)+(1/controls_exposed)+
+		  (1/cases_unexposed)+(1/controls_unexposed))
+     
+    z <- qnorm(1 - (alpha/2))
+    ci <- odds_ratio * c(exp(-z * sigma), exp( z * sigma))
+    
+    results[i, c("odds_ratio", "ci_low", "ci_upp")] <- c(odds_ratio, ci[1], ci[2])
+    
   }
+  results
 }
